7d91ec40199974627a36e88e265378bb9c0077a7
Tameshwar Nirmalkar <tameshwar.nirmalkar@mastercard.com>
2017-10-25 17:49:39 +0530

    refactor code

--- server/routes/acquirer.js
+++ server/routes/acquirer.js
@@ -1,11 +1,7 @@
-/**
- * Created by e062110 on 8/1/16.
- */
-
 const router = require('express').Router();
 const statusCode = require('../utils/helpers.js').statusCodes;
 const log = require('../utils/helpers.js').logRequest;
-//const _ = require('lodash');
+const _ = require('lodash');
 
 ///////////
 // Debug //
@@ -19,10 +15,9 @@ const acquirers = require('../data/acquirer.json');
 
 // Get all countries
 router.get('/acquirer/v0/acquirers', function (req, res) {
-    if(debug) log(req);
+    if (debug) log(req);
 
     res.status(statusCode.OK).json(acquirers);
 });
 
 module.exports = router;
-
